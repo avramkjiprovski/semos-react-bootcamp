1382825ef2c60ac732c2b04cde08d5ca6396e76d
avramkjiprovski <avramkjiprovski@gmail.com>
2021-11-10 14:18:38 +0100

    Update Students.js

--- homework-01/src/components/Students.js
+++ homework-01/src/components/Students.js
@@ -2,15 +2,11 @@ import React from 'react';
 import {Student} from './Student';
 
 export const Students = ({ students }) => {
-    let studentsList = []
-    students.map(student => {
-        studentsList.push(student)
-    })
     return (
         <div>
             <h1>Students</h1>
             {
-                studentsList.map(student => 
+                students.map(student => 
                     <div>
                         <Student key={student.index} student={student} />
                         <br/>
